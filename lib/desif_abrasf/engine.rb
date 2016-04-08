b427fde20935f057ee19b4aff80fa8ef047a7d5a
Marco Moura <marco.moura@gmail.com>
2016-04-08 12:32:55 -0300

    Rspec added

--- lib/desif_abrasf/engine.rb
+++ lib/desif_abrasf/engine.rb
@@ -1,5 +1,9 @@
 module DesifAbrasf
   class Engine < ::Rails::Engine
     isolate_namespace DesifAbrasf
+
+    config.generators do |g|
+      g.test_framework :rspec
+    end
   end
 end
---
+++ spec/desif_spec.rb
@@ -0,0 +1,7 @@
+require 'spec_helper'
+
+describe DesifAbrasf do
+  it do
+    expect(DesifAbrasf::VERSION).to be_eql '0.0.1'
+  end
+end
