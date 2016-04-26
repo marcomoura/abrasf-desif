f6156717c72632b923321aab8161376f21ed9042
Marco Moura <marco.moura@gmail.com>
2016-04-26 17:26:16 -0300

    Added decorators configuration

--- lib/abrasf/desif/engine.rb
+++ lib/abrasf/desif/engine.rb
@@ -14,6 +14,12 @@ module Abrasf
         end
       end
 
+      config.to_prepare do
+        Dir.glob(Rails.root + "app/decorators/**/*_decorator*.rb").each do |c|
+          require_dependency(c)
+        end
+      end
+
       config.generators do |g|
         g.test_framework :rspec, fixture: false
         g.assets false
