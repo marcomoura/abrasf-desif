54d797f074e21a8d08628a29663d67735e0771f5
Marco Moura <marco.moura@gmail.com>
2016-06-08 14:52:18 -0300

    Add DB index to service_item reference at tax_code

--- db/migrate/20160412200143_create_abrasf_desif_tax_codes.rb
+++ db/migrate/20160412200143_create_abrasf_desif_tax_codes.rb
@@ -2,7 +2,7 @@ class CreateAbrasfDesifTaxCodes < ActiveRecord::Migration
   def change
     create_table :abrasf_desif_tax_codes do |t|
       t.string :description, limit: 200
-      t.references :abrasf_desif_service_item, foreign_key: true
+      t.references :abrasf_desif_service_item, foreign_key: true, index: true, null: false
     end
   end
 end
